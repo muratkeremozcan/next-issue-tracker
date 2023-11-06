1374b87a637f23acb6bd3a07f02cef8c91b96261
Murat Ozcan <muratkerem@gmail.com>
2023-11-06 09:16:45 -0600

    ct [ci skip]

--- app/issues/loadingLoadingIssuesPage.cy.tsx
+++ app/issues/loadingLoadingIssuesPage.cy.tsx
@@ -4,5 +4,8 @@ describe('<LoadingIssuesPage />', () => {
   it('renders', () => {
     cy.mount(<LoadingIssuesPage />)
     cy.getByCy('issue-actions-comp').should('be.visible')
+    cy.contains('Issue')
+    cy.contains('Status')
+    cy.contains('Created')
   })
 })
