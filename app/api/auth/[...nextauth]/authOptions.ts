0c565e7e66438bd63a2b791f7701d2d964389661
Murat Ozcan <muratkerem@gmail.com>
2023-11-15 08:02:45 -0600

    section 6.3

--- app/api/auth/[...nextauth]/authOptions.ts
+++ app/api/auth/[...nextauth]/authOptions.ts
@@ -1,8 +1,11 @@
 import GoogleProvider from 'next-auth/providers/google'
+import {PrismaAdapter} from '@next-auth/prisma-adapter'
+import {prisma} from '@/prisma/client'
 
 // export the options so we can use them
 // to access the session at the server, at Home component
 export const authOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
--- prisma/migrations/20231101130259_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Issue" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "title" TEXT NOT NULL,
-    "description" TEXT NOT NULL,
-    "status" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAT" DATETIME NOT NULL
-);
