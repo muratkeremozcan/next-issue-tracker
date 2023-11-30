40ab371f74e663c8577040fc951b87321e42cfba
Murat Ozcan <muratkerem@gmail.com>
2023-11-30 08:01:54 -0600

    section8.2

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,20 @@
 import LatestIssues from './LatestIssues'
+import IssueSummary from './IssueSummary'
+import {prisma} from '@/prisma/client'
+import type {Status} from './issues/types'
+import {Flex} from '@radix-ui/themes'
 
-export default function Home() {
-  return <LatestIssues />
+const getIssueCount = (status: Status) => prisma.issue.count({where: {status}})
+
+export default async function Home() {
+  const open = await getIssueCount('OPEN')
+  const inProgress = await getIssueCount('IN_PROGRESS')
+  const closed = await getIssueCount('DONE')
+
+  return (
+    <Flex gap="4" direction="column">
+      <IssueSummary open={open} inProgress={inProgress} closed={closed} />
+      <LatestIssues />
+    </Flex>
+  )
 }
