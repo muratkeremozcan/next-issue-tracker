ff6c1988f45936dac17653251f3463d50fe575a3
Murat Ozcan <muratkerem@gmail.com>
2024-03-22 09:20:28 -0500

    attemp to fix cyct css

--- cypress/support/component.tsx
+++ cypress/support/component.tsx
@@ -2,7 +2,7 @@
 // https://github.com/cypress-io/cypress/issues/27890
 // import '@radix-ui/themes/styles.css'
 // import '../../app/theme-config.css'
-// import '../../app/globals.css'
+import '../../app/globals.css'
 import './commands'
 import {mount} from 'cypress/react18'
 import {Theme} from '@radix-ui/themes'
